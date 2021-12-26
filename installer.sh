8fd47379f2fc51c95f770817ddda245cdd833f46
Gabriel Dantas <63476124+gdma2004@users.noreply.github.com>
2021-12-26 19:03:50 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,13 @@
 sudo clear
 
-echo Downloading\n
+echo Downloading
 
-cd ~/Downloads && wget https://iriun.gitlab.io/iriunwebcam-2.6.deb
+cd ~/Downloads && wget -r --no-parent -A "iriunwebcam-*.deb" https://iriun.gitlab.io/
 mkdir ~/Downloads/Iriun
-mv ~/Downloads/iriunwebcam-*.deb ~/Downloads/Iriun
+mv ~/Downloads/iriun.gitlab.io/iriunwebcam-*.deb ~/Downloads/Iriun
 cd ~/Downloads/Iriun && ar vx ~/Downloads/Iriun/iriunwebcam-*.deb
 cd ~/Downloads/Iriun && tar -xf ~/Downloads/Iriun/data.tar.xz
 sudo mv ~/Downloads/Iriun/usr/local/bin/iriunwebcam /usr/bin
-rm -rf ~/Downloads/Iriun
+rm -rf ~/Downloads/Iriun && rm -rf ~/Downloads/iriun.gitlab.io
 
-echo \nDone, run it by typing iriunwebcam.
+echo Done, run it by typing iriunwebcam
